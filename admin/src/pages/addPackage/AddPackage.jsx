54c230a25e52e76ce064969599cd0e46b0a6511b
Reza <alialreza98@gmail.com>
2022-06-21 00:52:42 +0600

    add package successful

--- admin/src/pages/addPackage/AddPackage.jsx
+++ admin/src/pages/addPackage/AddPackage.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./addPackage.scss";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import BaseUrl from "../../api/BaseUrl";
 
 const AddPackage = () => {
+  const navigate = useNavigate();
+  const [info, setInfo] = useState({
+    package: undefined,
+    descriptions: undefined,
+    amount: undefined,
+    member_id: undefined,
+  });
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (info.package === undefined) {
+      return alert("Package name Not be empty");
+    } else if (info.descriptions === undefined) {
+      return alert("description  Not Be Empty");
+    } else if (info.amount === undefined) {
+      return alert("amount must not be empty");
+    } else if (info.member_id === undefined) {
+      return alert("member id must not Be Empty");
+    } else {
+      console.log({ info: info });
+      try {
+        await BaseUrl.post("/packages", info);
+        navigate("/packages");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div className="adduser">
       <Sidebar />
@@ -32,44 +70,53 @@ const AddPackage = () => {
               </div>
               <div className="formInput">
                 <TextField
-                  id=""
+                  id="package"
                   label="Package"
                   variant="standard"
                   placeholder="Package"
+                  onChange={handleChange}
                 />
               </div>
               <div className="formInput">
                 <TextField
-                  id=""
+                  id="descriptions"
                   label="Package Description"
                   variant="standard"
                   placeholder="description"
+                  onChange={handleChange}
                 />
               </div>
               <div className="formInput">
                 <TextField
-                  id=""
+                  id="amount"
                   label="Amount"
                   type="number"
                   InputLabelProps={{
                     shrink: true,
                   }}
                   variant="standard"
+                  onChange={handleChange}
                 />
               </div>
               <div className="formInput">
                 <TextField
-                  id=""
+                  id="member_id"
                   label="Member Id"
                   type="number"
                   InputLabelProps={{
                     shrink: true,
                   }}
                   variant="standard"
+                  onChange={handleChange}
                 />
               </div>
               <div className="formInput">
-                <Button className="button" variant="contained" color="primary">
+                <Button
+                  onClick={handleClick}
+                  className="button"
+                  variant="contained"
+                  color="primary"
+                >
                   Submit
                 </Button>
               </div>
