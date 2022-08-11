7d1dfb8c71040d4f8a16c9e9f00e0076f491af69
Reza <alialreza98@gmail.com>
2022-08-12 03:16:28 +0600

    same proble with clint side

--- client/src/component/home.jsx
+++ client/src/component/home.jsx
@@ -4,36 +4,83 @@ import { AuthContext } from "../context/AuthContext";
 
 function Header() {
   const { user, loading } = useContext(AuthContext);
-  console.log({ loading, user });
+  // console.log({ loading, user });
+
+  // return (
+  //   <>
+  //     {loading ? (
+  //       <div id="main">
+  //         <div className="pr-heading">
+  //           <h2>STEP UP YOUR</h2>
+  //           <h1>
+  //             <span>FITNESS</span>WITH US
+  //           </h1>
+  //           <p className="details">
+  //             Buils Your Body And Fitness With Professional Touch
+  //           </p>
+  //           <div className="header-btns">
+  //             {!user ? (
+  //               <Link to={"/register"}>
+  //                 <button className="header-btn">JOIN US</button>
+  //               </Link>
+  //             ) : (
+  //               <Link to={`/user/47`}>
+  //                 <button className="header-btn">VIEW PROFILE</button>
+  //               </Link>
+  //             )}
+  //           </div>
+  //         </div>
+  //       </div>
+  //     ) : (
+  //       <div id="main">
+  //         <div className="pr-heading">
+  //           <h2>STEP UP YOUR</h2>
+  //           <h1>
+  //             <span>FITNESS</span>WITH US
+  //           </h1>
+  //           <p className="details">
+  //             Buils Your Body And Fitness With Professional Touch
+  //           </p>
+  //           <div className="header-btns">
+  //             {!user ? (
+  //               <Link to={"/register"}>
+  //                 <button className="header-btn">JOIN US</button>
+  //               </Link>
+  //             ) : (
+  //               <Link to={`/user/${user.data.id}`}>
+  //                 <button className="header-btn">VIEW PROFILE</button>
+  //               </Link>
+  //             )}
+  //           </div>
+  //         </div>
+  //       </div>
+  //     )}
+  //   </>
+  // );
+
   return (
-    <>
-      {loading ? (
-        <h1>Is loading</h1>
-      ) : (
-        <div id="main">
-          <div className="pr-heading">
-            <h2>STEP UP YOUR</h2>
-            <h1>
-              <span>FITNESS</span>WITH US
-            </h1>
-            <p className="details">
-              Buils Your Body And Fitness With Professional Touch
-            </p>
-            <div className="header-btns">
-              {!user ? (
-                <Link to={"/register"}>
-                  <button className="header-btn">JOIN US</button>
-                </Link>
-              ) : (
-                <Link to={`/user/${user.data.id}`}>
-                  <button className="header-btn">VIEW PROFILE</button>
-                </Link>
-              )}
-            </div>
-          </div>
+    <div id="main">
+      <div className="pr-heading">
+        <h2>STEP UP YOUR</h2>
+        <h1>
+          <span>FITNESS</span>WITH US
+        </h1>
+        <p className="details">
+          Buils Your Body And Fitness With Professional Touch
+        </p>
+        <div className="header-btns">
+          {!user ? (
+            <Link to={"/register"}>
+              <button className="header-btn">JOIN US</button>
+            </Link>
+          ) : (
+            <Link to={`/user/${user.user.id}`}>
+              <button className="header-btn">VIEW PROFILE</button>
+            </Link>
+          )}
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
 export default Header;
